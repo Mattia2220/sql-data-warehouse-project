13f6c81771c6e34d6d1acd6b9a08577171ebdb35
Mattia2220 <falchimat@gmail.com>
2026-04-08 15:21:55 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -25,15 +25,15 @@ CREATE TABLE silver.crm_prd_info (
 	dwh_create_date DATETIME DEFAULT GETDATE()
 );
 
-IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+  IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details
 CREATE TABLE silver.crm_sales_details (
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT,
